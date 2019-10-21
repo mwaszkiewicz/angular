9158feb29ed05e7c5784b951f5f4381b1c72eeaf
Mateusz Waszkiewicz <mateusz.waszkiewicz@codetain.co>
2019-10-21 21:40:25 +0200

    integrate node-forge

--- certificate-app-backend/src/domain/certificate/certificate.controller.ts
+++ certificate-app-backend/src/domain/certificate/certificate.controller.ts
@@ -1,11 +1,24 @@
-import { Controller, Get, Post, Body, Param, Delete, Put } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete, Put, Res, HttpStatus } from '@nestjs/common';
+import { CertificateService } from './certificate.service';
+import { Request, Response } from 'express';
 
 @Controller('certificate')
 export class CertificateController {
+
+    constructor(private readonly certificateService: CertificateService) {
+      }
+
     @Get()
-    getAll() {
-        return 'we get all certificate';
+    getAll(@Res() res: Response) : Response {
+        let cert = this.certificateService.create('Fake Certificate');
+        return res.status(HttpStatus.OK).json( { result: cert});
     }
+
+    // @Get()
+    // async getAll(@Req() request: Request) : Promise<Certificate[]> {
+    //     console.log(request);
+    //     return this.certificateService.findAll();
+    // }
 
     @Post()
     create(@Body() certificateDto: string) {
--- certificate-app-backend/src/domain/certificate/certificate.service.ts
+++ certificate-app-backend/src/domain/certificate/certificate.service.ts
@@ -2,6 +2,8 @@ import { Injectable }       from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository }       from 'typeorm';
 import { Certificate }      from './certificate.entity';
+import * as forge           from "node-forge";
+
 
 @Injectable()
 export class CertificateService {
@@ -12,5 +14,85 @@ export class CertificateService {
 
       findAll(): Promise<Certificate[]> {
         return this.certificateRepository.find();
+      }
+
+      create(name : string): string {
+        let pki = forge.pki;
+        let keys = pki.rsa.generateKeyPair(2048);
+        let cert = pki.createCertificate();
+        cert.publicKey = keys.publicKey;
+        cert.serialNumber = '01';
+        cert.validity.notBefore = new Date();
+        cert.validity.notAfter = new Date();
+        cert.validity.notAfter.setFullYear(cert.validity.notBefore.getFullYear() + 1);
+        var attrs = [{
+          name: 'commonName',
+          value: 'example.org'
+        }, {
+          name: 'countryName',
+          value: 'US'
+        }, {
+          shortName: 'ST',
+          value: 'Virginia'
+        }, {
+          name: 'localityName',
+          value: 'Blacksburg'
+        }, {
+          name: 'organizationName',
+          value: 'Test'
+        }, {
+          shortName: 'OU',
+          value: 'Test'
+        }];
+        cert.setSubject(attrs);
+        cert.setIssuer(attrs);
+        cert.setExtensions([{
+          name: 'basicConstraints',
+          cA: true
+        }, {
+          name: 'keyUsage',
+          keyCertSign: true,
+          digitalSignature: true,
+          nonRepudiation: true,
+          keyEncipherment: true,
+          dataEncipherment: true
+        }, {
+          name: 'extKeyUsage',
+          serverAuth: true,
+          clientAuth: true,
+          codeSigning: true,
+          emailProtection: true,
+          timeStamping: true
+        }, {
+          name: 'nsCertType',
+          client: true,
+          server: true,
+          email: true,
+          objsign: true,
+          sslCA: true,
+          emailCA: true,
+          objCA: true
+        }, {
+          name: 'subjectAltName',
+          altNames: [{
+            type: 6, // URI
+            value: 'http://example.org/webid#me'
+          }, {
+            type: 7, // IP
+            ip: '127.0.0.1'
+          }]
+        }, {
+          name: 'subjectKeyIdentifier'
+        }]);
+
+        cert.sign(keys.privateKey);
+
+        // convert a Forge certificate to PE
+        let pem = pki.certificateToPem(cert);
+
+        // convert a Forge certificate from PEM
+        let certFromPem = pki.certificateFromPem(pem);
+        console.log(certFromPem.subject);
+        return JSON.stringify(certFromPem.issuer.attributes, null, 2)
       }
 }
